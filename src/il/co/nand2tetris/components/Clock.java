866317a528547cc71b3147759d0f6006937063b6
Roy Ash <roy.ash456@gmail.com>
2020-01-25 21:35:39 +0200

    added sequentialGates and did few refactoring

--- src/il/co/nand2tetris/components/Clock.java
+++ src/il/co/nand2tetris/components/Clock.java
@@ -5,49 +5,60 @@ import java.util.List;
 
 public class Clock
 {
-	private final List<SequentialGate> m_lListeners = new LinkedList<>();
-	private boolean m_bUp = true;
-	private final static Clock m_cClock = new Clock();
+	private final static List<SequentialGate> m_lListeners = new LinkedList<>();
+	private static boolean m_bUp = true;
+//	private final static Clock m_cClock = new Clock();
 
 	private Clock()
 	{
 	}
 
-	private void register(SequentialGate g)
-	{
-		m_lListeners.add(g);
-	}
+//	private void register(SequentialGate g)
+//	{
+//		m_lListeners.add(g);
+//	}
 
 	public static void RegisterSequentialGate(SequentialGate g)
 	{
-		m_cClock.register(g);
+		m_lListeners.add(g);
+//		m_cClock.register(g);
 	}
 
-	private void up()
+//	private void up()
+//	{
+//		if (!m_bUp)
+//		{
+//			m_lListeners.forEach(SequentialGate::onClockUp);
+//			m_bUp = true;
+//		}
+//	}
+//
+//	private void down()
+//	{
+//		if (m_bUp)
+//		{
+//			m_lListeners.forEach(SequentialGate::onClockDown);
+//			m_bUp = false;
+//		}
+//	}
+
+	public static void clockUp()
 	{
 		if (!m_bUp)
 		{
 			m_lListeners.forEach(SequentialGate::onClockUp);
 			m_bUp = true;
 		}
+//		m_cClock.up();
 	}
 
-	private void down()
+	public static void clockDown()
 	{
 		if (m_bUp)
 		{
 			m_lListeners.forEach(SequentialGate::onClockDown);
 			m_bUp = false;
 		}
-	}
-
-	public static void clockUp()
-	{
-		m_cClock.up();
-	}
-
-	public static void clockDown()
-	{
-		m_cClock.down();
+//		m_cClock.down();
 	}
 }
